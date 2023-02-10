593e090b7c4c9bee0f4088eb1a5ea2d28e6b814e
UgurBerkecan <UgurBerkecan>
2023-02-10 13:14:57 +0300

    cors updated

--- Api/config/express.js
+++ Api/config/express.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const routes = require('../routes.js');
+const cors = require('cors');
 
 app.use(session({
   secret: 'mySecretKeyyy',
@@ -9,6 +10,7 @@ app.use(session({
   saveUninitialized: false
 }));
 
+app.use(cors({credentials: true, origin: true}));
 
 
 app.use(express.json());
